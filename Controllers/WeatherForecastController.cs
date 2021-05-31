8ce7dbd6f66efd3b17463f3d3033a80a25a4bfe1
Guilherme Vinicius Pedroso Pedroza <bherewow@gmail.com>
2021-05-31 20:49:53 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,17 +23,10 @@ namespace Projeto_C_.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPut]
+        public String Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Hello World!";
         }
     }
 }
