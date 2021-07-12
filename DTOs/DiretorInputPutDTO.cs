e4abb428c97af892fd966c97ad13dfa864978e5a
Guilherme Vinicius Pedroso Pedroza <bherewow@gmail.com>
2021-07-12 20:53:41 -0300

    Aula 8.1

--- DTOs/DiretorInputPutDTO.cs
+++ DTOs/DiretorInputPutDTO.cs
@@ -1,3 +1,19 @@
+using System;
+using FluentValidation;
+
 public class DiretorInputPutDTO {
      public string Nome { get; set; }
+}
+public class DiretorInputPutDTOValidator : AbstractValidator<DiretorInputPutDTO>{
+
+    public DiretorInputPutDTOValidator(){
+        RuleFor(diretor => diretor.Nome).NotNull().NotEmpty();
+        RuleFor(diretor => diretor.Nome).Length(2,250).WithMessage("Tamanho informado {TotalLength} é Invalido");
+
+    }
+
+    private object RuleFor(Func<object, object> p)
+    {
+        throw new NotImplementedException();
+    }
 }
--- DTOs/FilmeInputPost.cs
+++ DTOs/FilmeInputPost.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 public class FilmeInputPostDTO {
      public string Titulo { get; set; }
      public long DiretorID { get; set; }
@@ -7,4 +9,13 @@ public class FilmeInputPostDTO {
           DiretorID = id;
      }
      
+}
+
+public class MovieInputPostDTOValidator : AbstractValidator<FilmeInputPostDTO> {
+    public MovieInputPostDTOValidator() {
+        RuleFor(f=> f.Titulo).NotNull().NotEmpty();
+        RuleFor(f => f.Titulo).Length(2, 250).WithMessage("Tamanho {TotalLength} é inválido");
+        RuleFor(f=> f.DiretorID).NotNull().NotEmpty();
+
+    }
 }
