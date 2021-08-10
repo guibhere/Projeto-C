4e015486ac4a521f569f47732f60ffd1ac1aff25
Guilherme Vinicius Pedroso Pedroza <bherewow@gmail.com>
2021-08-09 21:17:56 -0300

    Testes

---
+++ MeuAppTestes/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace MeuAppTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
